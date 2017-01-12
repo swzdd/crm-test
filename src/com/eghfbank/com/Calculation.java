20a5bd166954001a818d248d98fe738eb5b3101a
sunweizhen <sunweizhenswz@hfbank.com.cn>
2017-01-12 16:33:17 +0800

    测试1

---
+++ src/com/eghfbank/com/Calculation.java
@@ -0,0 +1,10 @@
+package com.eghfbank.com;
+
+public class Calculation {
+
+    public static void main(String[] args) {
+        int a =1;
+        int b=2;
+        System.out.println(a+b);
+    }
+}
